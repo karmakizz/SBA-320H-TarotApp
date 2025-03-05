92b2e87624f22f37905bf97e31f4593fec48a99f
Iris Lopez <iris.lopez.co@gmail.com>
2025-03-05 15:45:23 -0700

    Set port for Vite to 80

--- TarotApp/vite.config.js
+++ TarotApp/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 80, // Specify the port that Render expects (usually 80 for production)
+  },
 })
